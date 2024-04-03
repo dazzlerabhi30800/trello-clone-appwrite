e76c3dc15f187303336aa1abe6f8f7e0b1fbc8ba
= <abhisingh30800@gmail.com>
2024-04-03 12:33:37 +0530

    removed api folder & open ai dependencies

--- components/openai.ts
+++
@@ -1,8 +0,0 @@
-import OpenAI from "openai";
-
-const openai = new OpenAI({
-  // organization: "org-5ysCR1kcFMWB5lVVEN0YuMhG",
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
-});
-
-export default openai;
